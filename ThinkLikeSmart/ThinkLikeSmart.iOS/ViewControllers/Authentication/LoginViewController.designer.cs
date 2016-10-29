3bd4691ffa19d251b888b2e31efbe38b63effd06
Yevgeny Sotnikov <esotnikov@playtika.com>
2016-10-29 15:54:47 +0300

    added first version of login gui screen for ios

---
+++ ThinkLikeSmart/ThinkLikeSmart.iOS/ViewControllers/Authentication/LoginViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Tls.ThinkLikeSmart.iOS.ViewControllers.Authentication
+{
+    [Register ("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Action ("UIButton20_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton20_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("UIButton21_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton21_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
