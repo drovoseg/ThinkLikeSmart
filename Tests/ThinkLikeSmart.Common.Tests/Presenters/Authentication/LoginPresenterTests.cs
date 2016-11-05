c53afae4f3056ec3895596917b23fff9b56c81df
Yevgeny Sotnikov <yevgeny.sotnikov@gmail.com>
2016-11-05 19:05:35 +0200

    added tests project for ThinkLikeSmart.Common PCL module

---
+++ Tests/ThinkLikeSmart.Common.Tests/Presenters/Authentication/LoginPresenterTests.cs
@@ -0,0 +1,87 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using Tls.ThinkLikeSmart.Common.Presenters.Authentication;
+using Tls.ThinkLikeSmart.Common.Storage;
+using Tls.ThinkLikeSmart.Common.Views;
+using Tls.ThinkLikeSmart.Common.Views.Authentication;
+
+namespace ThinkLikeSmart.Common.Tests.Presenters.Authentication
+{
+    [TestFixture]
+    public class LoginPresenterTests
+    {
+        private LoginPresenter loginPresenter;
+        private ISettings settings;
+        private ILoginView loginView;
+
+        [SetUp]
+        public void SetUp()
+        {
+            settings = Substitute.For<ISettings>();
+            loginView = Substitute.For<ILoginView>();
+
+            loginPresenter = new LoginPresenter(loginView, settings);
+        }
+
+        [Test]
+        public void Constructor_ViewParamIsNull_ShouldArgumentNullExceptionThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new LoginPresenter(null, settings));
+        }
+
+        [Test]
+        public void Constructor_SettingsParamIsNull_ShouldArgumentNullExceptionThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new LoginPresenter(loginView, null));
+        }
+
+        [TestCase(LoginType.Phone, VisibliltyMode.Visible)]
+        [TestCase(LoginType.Email, VisibliltyMode.Gone)]
+        public void ViewCreated_RecentLoginTypeIsPhone_CountriesContainerShouldBeSetProperly(LoginType arrangedLoginType, VisibliltyMode expectedMode)
+        {
+            //Arrange
+            settings.RecentLoginType.Returns(arrangedLoginType);
+
+            //Act
+            loginPresenter.ViewCreated();
+
+            //Assert
+            loginView.Received().CountriesContainerVisiblity = expectedMode;
+        }
+
+        [Test]
+        public void ViewCreated_RecentLoginTypeIsPhone_TogglePhoneLoginTypeShouldBeCalled()
+        {
+            //Arrange
+            settings.RecentLoginType.Returns(LoginType.Phone);
+
+            //Act
+            loginPresenter.ViewCreated();
+
+            //Assert
+            loginView.Received().TogglePhoneLoginType();
+        }
+        
+        [Test]
+        public void ViewCreated_RecentLoginTypeIsEmail_ToggleEmailLoginTypeShouldBeCalled()
+        {
+            //Arrange
+            settings.RecentLoginType.Returns(LoginType.Email);
+
+            //Act
+            loginPresenter.ViewCreated();
+
+            //Assert
+            loginView.Received().ToggleEmailLoginType();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            settings = null;
+            loginView = null;
+            loginPresenter = null;
+        }
+    }
+}
