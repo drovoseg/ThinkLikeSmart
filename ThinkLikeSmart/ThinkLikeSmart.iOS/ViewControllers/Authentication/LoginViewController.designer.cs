c98fe3a16f9651ba6639dafdcfe4e739c979ada4
Yevgeny Sotnikov <yevgeny.sotnikov@gmail.com>
2016-11-07 02:01:23 +0200

    add *.designer.cs for ios

---
+++ ThinkLikeSmart/ThinkLikeSmart.iOS/ViewControllers/Authentication/LoginViewController.designer.cs
@@ -0,0 +1,93 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using System.Drawing;
+using UIKit;
+
+namespace Tls.ThinkLikeSmart.iOS.ViewControllers.Authentication
+{
+    [Register ("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView countyControlsContainerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton forgetPasswordButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISegmentedControl loginMethodSegmentedControl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField loginTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField passwordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton registrationButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton rememberPasswordButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (countyControlsContainerView != null) {
+                countyControlsContainerView.Dispose ();
+                countyControlsContainerView = null;
+            }
+
+            if (forgetPasswordButton != null) {
+                forgetPasswordButton.Dispose ();
+                forgetPasswordButton = null;
+            }
+
+            if (loginButton != null) {
+                loginButton.Dispose ();
+                loginButton = null;
+            }
+
+            if (loginMethodSegmentedControl != null) {
+                loginMethodSegmentedControl.Dispose ();
+                loginMethodSegmentedControl = null;
+            }
+
+            if (loginTextField != null) {
+                loginTextField.Dispose ();
+                loginTextField = null;
+            }
+
+            if (passwordTextField != null) {
+                passwordTextField.Dispose ();
+                passwordTextField = null;
+            }
+
+            if (registrationButton != null) {
+                registrationButton.Dispose ();
+                registrationButton = null;
+            }
+
+            if (rememberPasswordButton != null) {
+                rememberPasswordButton.Dispose ();
+                rememberPasswordButton = null;
+            }
+        }
+    }
+}
